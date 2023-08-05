9fd97cbe8c765a07eb0e0018f357ae7a23418483
Angus <angus.goody@gmail.com>
2023-08-05 10:59:27 +0100

    testing new permissions

--- start_prod.sh
+++ start_prod.sh
@@ -2,28 +2,21 @@
 
 
 # Create .env file from environment variables
-RUN printenv | grep -v "no_proxy" > .env
+printenv | awk -F "=" 'NF==2 && $2 !~ /[\n\t ]/' > .env
 
 # Run our artisan commands
 php artisan route:clear
-
-php artisan route:cache
-
 php artisan config:clear
-
-php artisan config:cache
-
 php artisan view:clear
-
-php artisan view:cache
 
 php artisan storage:link
 
 php artisan migrate --force
 
-php artisan optimize
+# Set folder permission
+chown -R www-data:www-data storage
+chmod -R 755 storage
 
-chmod -R 777 storage
 
 # Finally, start PHP-FPM and nginx
 php-fpm -D &&  nginx -g "daemon off;"
