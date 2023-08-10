fcc381ff0298bec6efb1455864586d1b22ab1c19
Angus <angus.goody@gmail.com>
2023-08-10 22:11:41 +0100

    trying to fix bug again

--- start_prod.sh
+++ start_prod.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-
 # Create .env file from environment variables
 printenv | awk -F "=" 'NF==2 && $2 !~ /[\n\t ]/' > .env
+
+# Set php-fpm permissions
+sed -i 's/^user =.*/user = www-data/' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's/^group =.*/group = web/' /usr/local/etc/php-fpm.d/www.conf
 
 # Run our artisan commands
 php artisan route:clear
@@ -12,6 +15,9 @@ php artisan view:clear
 php artisan storage:link
 
 php artisan migrate --force
+
+# Set the desired umask
+umask 0002
 
 # Finally, start PHP-FPM and nginx
 php-fpm -D &&  nginx -g "daemon off;"
